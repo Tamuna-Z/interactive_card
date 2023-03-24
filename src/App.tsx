4285c71d12a66001969ca7b802599bbc70580a0b
lizi mazanishvili <90831707+Tamuna-Z@users.noreply.github.com>
2023-03-24 22:51:50 +0400

    add form and data on cards

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import backCard from './images/bg-card-back.png';
 import frontCard from './images/bg-card-front.png';
+import logo from './images/card-logo.svg'
 
 function App() {
   return (
@@ -10,10 +11,33 @@ function App() {
         <div className="cardContainer">
           <div className="colorBg">
             <img className="backCard" src={backCard} alt="back_card"/>
-            <img className="frontCard" src={frontCard}/>
+            <img className="frontCard" src={frontCard} />
+            <img className="logoCard" src={logo}/>
+            <div className="cardData">
+              <h2>0000 0000 0000 0000</h2>
+              <ul>
+                <li>Tamar Zurabashvili</li>
+                <li>00/00</li>
+              </ul>
+            </div>
           </div>
         </div>
         <div className="formContainer">
+          <form>
+            <div className="inputName">
+            <label>Cardholder Name</label>
+            <input type="text" placeholder="e.g. Jane Appleseed"/>
+            </div>
+            <div className="inputNumber">
+            <label>Card Number</label>
+            <input type="number" placeholder="e.g. 1234 5678 9123 0000"/>
+            </div>
+            <div className="inputDate">
+            <label>Exp. Date (MM/YY)</label> <span>CVC</span>
+            <input type="date" placeholder="e.g. Jane Appleseed"/>
+            </div>
+          </form>
+
 
         </div>
       </div>
